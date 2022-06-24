ab12495106bdedaae986a4ea3e33442acc83b8d4
Leonardo melo <44414965+lxnvrdx@users.noreply.github.com>
2022-06-23 23:57:49 -0300

    event and lessons

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import Icon from '../components/404';
 import { Navbar } from '../components/Navbar';
-import { Player } from '../components/Player';
+import { PlayerVideo } from '../components/Player';
 import { Sidebar } from '../components/Sidebar';
 
 export function Event() {
+  const { slug } = useParams<{slug:string}>()
+
   return (
     <div className='flex flex-col min-h-screen'>
     <Navbar/>
     <main className='flex flex-1'>
-      <Player />
+      {slug ? <PlayerVideo lessonSlug={slug}/> :
+       <div className='flex-1 flex justify-center items-center flex-col'>
+        <h1 className='text-4xl font-bold uppercase'>Ops, nada encontrado aqui!</h1>
+        <div className='mt-16  mx-auto'>
+          <Icon  />
+        </div>
+        </div>}
       <Sidebar/>
     </main>
     </div>
